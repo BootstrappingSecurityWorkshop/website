8b9850aa8ff5c2ceb68df93b52d91d717bd73c9b
v1x1e <github@victoriahchang.com>
2017-11-22 11:28:00 -0800

    Config webpack

---
+++ webpack.config-min.js
@@ -0,0 +1,1 @@
+const path=require("path");var webpack=require("webpack");module.exports={devtool:"cheap-module-source-map",context:path.join(__dirname,"src"),entry:{index:"./loadIndex.js"},output:{path:path.join(__dirname,"dist"),filename:"[name].bundle.js"},module:{rules:[{test:/\.jsx?$/,exclude:/node_modules/,use:["babel-loader"]}]},resolve:{modules:[path.join(__dirname,"node_modules")]}};
