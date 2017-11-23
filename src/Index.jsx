12f4d6fc44561f5ca5fcbd3f4abd7bce36cf92fe
v1x1e <github@victoriahchang.com>
2017-11-23 09:54:57 -0800

    Add and render Index component

---
+++ src/Index.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+class Index extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  render() {
+    return (
+      <div id="index" className="container column">
+        <div>RogueGenius</div>
+      </div>
+    );
+  }
+}
+
+export default Index;
---
+++ src/loadIndex.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Index from './Index.jsx';
+
+document.addEventListener('DOMContentLoaded', () => {
+  ReactDOM.render(
+    <Index/>,
+    document.getElementById('index')
+  )
+});
