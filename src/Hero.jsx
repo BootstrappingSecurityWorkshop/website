ca9004d89999b3172bfe6cf22cab1ed2bf5b8f77
v1x1e <github@victoriahchang.com>
2017-11-23 21:03:15 -0800

    Add and style Section01 component

--- src/Hero.jsx
+++ src/Hero.jsx
@@ -2,9 +2,9 @@ import React from 'react';
 
 const Hero = () => {
   return (
-    <div className="hero bkgd-map bkgd-arrow container column section">
+    <div className="hero bkgd-map bkgd-arrow container column padding">
       <div className="hero-title">Bootstrapping Security</div>
-      <div className="hero-subtitle">Defend your company at startup speed.</div>
+      <div className="text">Defend your company at startup speed.</div>
       <img src="../dist/img/register-key.svg"/>
 
       <a className="register container column" href="###">
--- src/Index.jsx
+++ src/Index.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import Nav from './Nav.jsx';
 import Hero from './Hero.jsx';
+import Section01 from './Section01.jsx';
+import Section02 from './Section02.jsx';
 
 export default class Index extends React.Component {
   constructor(props) {
@@ -13,6 +15,8 @@ export default class Index extends React.Component {
       <div id="index" className="container column">
         <Nav/>
         <Hero/>
+        <Section01/>
+        <Section02/>
       </div>
     );
   }
