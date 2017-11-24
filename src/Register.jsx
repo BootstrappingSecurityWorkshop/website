0f30a52b7e15acdcb5d189ed3976690c49b3e0ab
v1x1e <github@victoriahchang.com>
2017-11-23 23:01:52 -0800

    Add and style Register component

---
+++ src/Register.jsx
@@ -0,0 +1,49 @@
+import React from 'react';
+
+const Register = () => {
+  return (
+    <div className="register container column padding">
+      <div className="title">Course Registration</div>
+
+      <div className="container row">
+        <div className="list">
+          <div className="list-icon icon-price">
+            <span>$999</span>
+          </div>
+
+          <div className="register-info">
+            <div className="register-title highlight">January 1, 1337</div>
+            <div className="register-text">Event Name</div>
+            <div className="register-text">San Francisco, CA</div>
+          </div>
+        </div>
+
+        <div className="list">
+          <div className="list-icon icon-price">
+            <span>$999</span>
+          </div>
+
+          <div className="register-info">
+            <div className="register-title highlight">January 1, 1337</div>
+            <div className="register-text">Event Name</div>
+            <div className="register-text">San Francisco, CA</div>
+          </div>
+        </div>
+
+        <div className="list">
+          <div className="list-icon icon-price">
+            <span>$999</span>
+          </div>
+
+          <div className="register-info">
+            <div className="register-title highlight">January 1, 1337</div>
+            <div className="register-text">Event Name</div>
+            <div className="register-text">San Francisco, CA</div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Register;
