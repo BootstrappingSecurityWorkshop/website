d08c49ea25f9ca9f99e61e4f9129517da2c23ee3
v1x1e <github@victoriahchang.com>
2017-11-23 18:57:22 -0800

    Add and style Nav component

---
+++ src/Nav.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const Nav = () => {
+  return(
+    <div className="nav container row">
+      <a href="###"><img src="../dist/img/register-key-sml.svg"/></a>
+      <a href="###">About</a>
+      <a href="###">Course</a>
+      <a href="###">Attendees</a>
+      <a href="###">Outcome</a>
+      <a href="###">FAQ</a>
+      <a href="###">George</a>
+      <a className="button" href="###">Register</a>
+    </div>
+  );
+};
+
+export default Nav;
