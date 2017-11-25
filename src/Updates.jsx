83352f99113e8c23a9058301e6522fcc1a1aea6a
v1x1e <github@victoriahchang.com>
2017-11-24 17:23:29 -0800

    Fix Netlify form handling

--- src/Updates.jsx
+++ src/Updates.jsx
@@ -7,7 +7,7 @@ const Updates = () => {
       <div className="title">Get Workshop Updates</div>
       <img src="/img/register-key.svg"/>
 
-      <form netlify className="signup container row" method="post" name="updates" action="">
+      <form className="signup container row" method="post" name="updates" action="" data-netlify="true">
         <input className="input-email" type="text" name="email" placeholder="Type your email here!"/>
         <input className="input-submit" type="submit" value="Submit"/>
       </form>
