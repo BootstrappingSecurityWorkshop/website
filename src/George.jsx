fb137dc60212b040fe605f788057bafe0e27af76
notgeorge <george@roguegenius.com>
2018-03-07 15:18:18 -0800

    years

--- src/George.jsx
+++ src/George.jsx
@@ -10,7 +10,7 @@ const George = () => {
       <div className="bio container row">
         <img className="bio-photo" src="/img/george.jpg"/>
         <div className="bio-text">
-        This workshop is the result of ten years of hands-on experience building security into companies ranging from five people to 50,000.  To get here I've worked as 
+        This workshop is the result of eighteen years of hands-on experience building security into companies ranging from five people to 50,000.  To get here I've worked as 
         a security engineer, security architect, programmer, dev/ops engineer, penetration tester, auditor and performed countless vulnerability assessments including
         security reviews of some of the largest, most complex critical infrastructure systems in the world.<br/><br/>
 
