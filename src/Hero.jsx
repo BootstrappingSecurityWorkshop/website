91c6f156a2a718a6c80045575b93c20ac190d801
notgeorge <george@roguegenius.com>
2018-04-18 08:34:31 -0700

    new tagline

--- src/Hero.jsx
+++ src/Hero.jsx
@@ -4,7 +4,7 @@ const Hero = () => {
   return (
     <div id="hero" className="hero bkgd-map container column padding">
       <div className="hero-title">Bootstrapping Security</div>
-      <div className="hero-text">Good security is a competitive advantage.</div>
+      <div className="hero-text">If you're qualified to build it, you're qualified to secure it.</div>
 
       <a className="register container column" href="#register">
         <span>Register</span>
